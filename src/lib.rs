dfce5ba753c113dc0493752a9714ba4fbfc233ab
martintc <kj4ntv@gmail.com>
2022-07-09 12:09:43 -0700

    Remove automatically generated test

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,6 @@ mod tests {
     use crate::twodarray;
 
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-
-    #[test]
     fn test_2darray() {
 	let arr = twodarray::Array2D::<u8>::new(2, 4);
 	let mut result = arr.get_height();
