fd2fe740b9968c2e6605fa2dd4f3b60af0982705
martintc <kj4ntv@gmail.com>
2022-07-09 12:02:38 -0700

    Initial test of library at lib level

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,20 @@ mod twodarray;
 
 #[cfg(test)]
 mod tests {
+    use crate::twodarray;
+
     #[test]
     fn it_works() {
         let result = 2 + 2;
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_2darray() {
+	let arr = twodarray::Array2D::<u8>::new(2, 4);
+	let mut result = arr.get_height();
+	assert_eq!(result, 4);
+	result = arr.get_width();
+	assert_eq!(result, 2);
     }
 }
